bb5b830280bda30806e0f16ac21207a34e29e853
Joey Massre <jm7217@nyu.edu>
2020-09-21 23:20:23 -0400

    Added sentiment analysis on Yankees World Series victory from NYT

---
+++ deliverable1.py
@@ -0,0 +1,98 @@
+import nltk
+
+from nltk.tokenize import sent_tokenize
+from nltk.probability import FreqDist
+from nltk.tokenize import word_tokenize
+import matplotlib.pyplot as plt
+
+text = """A sliver of time for other teams is an epoch for the Yankees, who define themselves by championships. For eight seasons, they led the majors in victories, payroll and drama. They built a ballpark, created a network and expanded their brand around the globe. But they did not win the World Series.
+
+Now they have done it. There is a 27th jewel in the Yankees’ crown and a peaceful, easy feeling across their empire. The Yankees captured their first title since 2000, humbling the defending champion Philadelphia Phillies on Wednesday, 7-3, in Game 6 of the World Series at Yankee Stadium.
+
+Hideki Matsui homered, with his six runs batted in tying a World Series record, and Andy Pettitte ground through five and two-thirds innings for his second victory in five days. Mariano Rivera collected the final five outs, getting Shane Victorino to ground out to second to end it.
+
+“They persevered and they were determined, a lot like the ’98 team,” General Manager Brian Cashman said, referring to the best Yankees team of modern times. “They had the attitude that nothing was going to stop them. But they had to prove it, and they proved it.”
+
+They did it on the eighth anniversary of Rivera’s lowest moment, when he blew Game 7 of the 2001 World Series in Arizona. The Yankees lost the World Series again two years later, to Florida, and they did not return until this season, fortifying their roster with free agents around the core of Rivera, Pettitte, Derek Jeter and Jorge Posada.
+
+“We play the game the right way,” Jeter said on the podium behind second base, cradling the Commissioner’s Trophy. “And we deserve to be standing here.”
+
+Journalism for every part of life.
+Subscribe for $1 a week.
+Pettitte became the second pitcher to win all three clinching games of a postseason. The other was Boston’s Derek Lowe in 2004, when the Yankees lost a three-games-to-none lead to the Red Sox, fumbling away a pennant and plunging into a postseason funk.
+
+Pettitte was gone that autumn, part of a three-year sojourn to his Houston hometown. Otherwise, Pettitte, Rivera, Jeter and Posada have been Yankees since 1995, through dynasty and drought and back to the top. The have each earned five championship rings, one more than Babe Ruth won for the Yankees, who will be honored with a parade in Manhattan on Friday.
+
+It is the seventh championship for the principal owner George Steinbrenner, 79, who was not at Yankee Stadium on Wednesday.
+
+Editors’ Picks
+
+The Best and Worst Moments of the 2020 Emmys
+
+Help! My Boss Is a Conspiracy Theorist
+
+Martha Stewart, Blissed Out on CBD, Is Doing Just Fine
+Continue reading the main story
+“It’s been a while  it’s been nine years,” said his son, Hal Steinbrenner, the managing general partner. “I just talked to him today. He was a little bit excited and nervous, as we all were. But this team, they fought and they fought and they fought. They never gave up.”
+
+Working on three days’ rest, Pettitte, 37, looked ragged at times with five walks and three strikeouts. But he held up better than the Phillies’ Pedro Martinez, 38, his old Boston adversary, who lasted just four innings.
+
+The Yankees managed just three hits off Martinez, who issued two walks, both becoming runs. The first was to Alex Rodriguez, on four pitches, leading off the second inning. It stopped Martinez’s momentum after a 1-2-3 first.
+
+Matsui fell behind, 0-2, but drew the count full while flicking two fouls. Martinez also threw to first twice. At worst, Matsui was driving up Martinez’s pitch count, following the plan against a brilliant but fragile pitcher. But he did much more than that.
+
+In Game 2, Matsui broke a 1-1 tie when he pulled a Martinez curveball over the right field wall in the sixth. This time, he took a fastball that tailed over the middle and slammed it inside the right field foul pole, just above a billboard for a Japanese construction-equipment company.
+
+Matsui, of course, has done more than raise the Yankees’ profile in Japan. He has been steady and efficient for seven seasons. His ravaged knees have made him a full-time designated hitter and called into doubt his future as he comes to the end of his contract. But if this was his last game as a Yankee, he made it his best.
+
+“I hope it works out,” Matsui said through an interpreter, referring to his future with the Yankees. “I love New York, I love the Yankees, and I love the fans here.”
+
+Matsui won three Japan Series for the Yomiuri Giants and was the most valuable player against the Daiei Hawks in 2000. He did it again against the Phillies, winning the M.V.P. by hitting .615 (8 for 13) with three homers and eight R.B.I.
+
+With one out and the bases loaded in the third, Martinez struck out Rodriguez on a pitch well off the outside corner. The Phillies were warming a left-hander, J.A. Happ, but Manager Charlie Manuel stuck with Martinez. Like Grady Little before him, Manuel paid for his faith.
+
+Again, Martinez got ahead of Matsui, 0-2. Catcher Carlos Ruiz bounced from his crouch for a target up and away. Martinez hit the spot but Matsui hit the pitch, lining it into left field for two runs.
+
+It swelled the Yankees’ lead to 4-1, and it gave Matsui nine hits in 19 postseason at-bats against Martinez. He also smacked a double off him in the eighth inning of Game 7 of the 2003 American League Championship Series, when Little, the Boston manager, refused to call for a reliever.
+
+Martinez lasted four more batters this time, retiring them all. Chad Durbin took over in the fifth, and Jeter, who finished 11 for 27 (.407), greeted him with a double. Mark Teixeira scored Jeter with a single, and after a walk, Happ came in.
+
+Matsui took four pitches from Happ, a rookie, three for balls. Then he ripped a double to deep right, scoring two runs to make it 7-1 and earning a share of a record. Only the Yankees’ Bobby Richardson, in 1960, had driven in six runs in a World Series game.
+
+Chase Utley, the Phillies’ second baseman, tied another Yankee in this World Series with five home runs, matching Reggie Jackson’s record haul in 1977. But Utley was hardly a factor in Game 6, grounding into a double play in the first inning, and striking out twice. The Phillies hit .227 in the six games.
+
+“I give credit to some of the Yankees’ pitching, but it seemed like our offense, when we had a chance to really get down and get the big hits, or we had to do things to kind of take them out of the game, it seemed like we couldn’t do it,” Manuel said. “We kind of sputtered a little bit.”
+
+Utley had just one hit besides his home runs. Ryan Howard finished the series 4 for 23 with a record 13 strikeouts. It took him until Wednesday to hit a home run, finally muscling a ball to the first row of seats in left-center with one out in the sixth.
+
+After an out and a double, Pettitte’s night was over. The fans had sensed the end all through the sixth, chanting Pettitte’s name, and they erupted when he jogged off the mound.
+
+Pettitte slowed near the dugout steps, lifting his cap and shaking it. He had labored to beat the Phillies on Saturday, saying that night he had never struggled so much in a postseason victory. Now he had won again, on fumes, doing just enough to topple a champion.
+
+“It was really nothing special, but he made one great pitch every at-bat, it seemed like, and when you get that one pitch, he was able to make it, get some double plays and really shorten the inning for a team,” said the Phillies’ Jimmy Rollins, who had predicted his team would win in five games.
+
+“That’s what Andy does  he keeps his team in the game, and you walk away shaking your head like, How didn’t we get to him? Obviously, he’s pretty good.”
+
+Joba Chamberlain and Damaso Marte followed Pettitte with scoreless relief. The Yankees adjusted their bullpen as the postseason went along, finding hot hands in Chamberlain and Marte, who got 17 outs while allowing two hits in the World Series.
+
+They brought the lead to Rivera, the indomitable closer and the greatest bullpen force in baseball history. The final out took 10 pitches, including four two-strike fouls from Victorino, a defending champion clinging to life.
+
+“Alex and I were just talking about it in the hallway,” said Teixeira, his championship T-shirt drenched in Champagne, in a room off the clubhouse. “All game long, it was one out at a time, one out at a time, and that really tested our patience, because he was fouling.”
+
+Teixeira said: “And those are great players. We beat the world champions. We beat an incredible team over there. They deserved the championship they got last year, and we deserve this one. That last out took a while.”
+
+A ground ball finally came, from Robinson Cano to Teixeira, who caught it and bolted across the diamond. It was all such a blur, Teixeira said, that he did not even know whom he found when he got there.
+
+It was Rodriguez, the slugger who had never won before, and he greeted the throng with his arms in the air. Decorated champions embraced new ones, christening the ballpark the way they all expected. The championship was back to the Bronx, where the Yankees believe it belongs.
+
+“This is what the Steinbrenner family has strived for year after year and tried to deliver to the city of New York,” Manager Joe Girardi said. “To be able to deliver this to the Boss, to the stadium he created and the atmosphere he created around here, it’s very gratifying to all of us.”"""
+
+
+
+tokenized_text = sent_tokenize(text)
+tokenized_word=word_tokenize(text)
+
+fdist = FreqDist(tokenized_word)
+
+fdist.plot(30,cumulative=False)
+plt.show()
